f8d84561103d252d236afd802c1bef1a90a298e7
Roman Nevyantsev <kovin44@gmail.com>
2019-05-21 01:08:31 +0300

    made some refactoring and added comments

--- src/main/kotlin/org/telegram/seconds_bot/BotController.kt
+++ src/main/kotlin/org/telegram/seconds_bot/BotController.kt
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Value
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
-import java.lang.Thread.sleep
 import java.util.*
 import java.util.logging.Level
 import java.util.logging.Logger
@@ -18,7 +17,6 @@ class BotController {
     companion object {
         private val API_ENDPOINT = "https://api.telegram.org/bot"
         private val START_COMMAND = "/start"
-        private val ECHO_COMMAND = "/echo"
         private val WAIT_COMMAND = "/wait"
     }
 
@@ -26,99 +24,160 @@ class BotController {
     @Value("\${token}")
     lateinit var token: String
 
+
+    /**
+     * checks if user's message had any known command and calls appropriate functions
+     */
     @PostMapping("/")
     fun onUpdate(@RequestBody update: Update) {
         logger.log(Level.INFO, "Got update: " + update)
         if (update.message != null) {
             val chatId = update.message.chat.id
-            val text= update.message.text
+            val text = update.message.text
             when {
                 text?.startsWith(WAIT_COMMAND) == true -> onWaitCommand(chatId, text)
                 text?.startsWith(START_COMMAND) == true -> onStartCommand(chatId)
-                text?.startsWith(ECHO_COMMAND) == true -> onEchoCommand(chatId, text)
             }
         }
     }
 
+
+    /**
+     * sends hello message on /start
+     */
     private fun onStartCommand(chatId: Long) = try {
         sendMessage(chatId, "Hello! I'm Seconds Waiting Bot.")
     } catch (e: UnirestException) {
         logger.log(Level.SEVERE, "Can not send START response!", e)
     }
 
-    private fun onEchoCommand(chatId: Long, text: String) = try {
-        val response = text.subSequence(ECHO_COMMAND.length, text.length).trim().toString()
-        sendMessage(chatId, response)
-    } catch (e: UnirestException) {
-        logger.log(Level.SEVERE, "Can not send ECHO response!", e)
-    }
 
+    /**
+     * chatId - id of chat
+     * text - text for bot to write to chat
+     *
+     * checks whether text has only digits and ':'
+     * chooses appropriate answer to user's request
+     * if format of request is correct then sends approval of request, waits required amount of seconds
+     * and sends message with confirmation of waiting for required time
+     * if format of request isn't corrent then sends denial of request
+     */
     private fun onWaitCommand(chatId: Long, text: String) = try {
         val textWithoutCommand = text.subSequence(WAIT_COMMAND.length, text.length).trim().toString()
-        if (textWithoutCommand.all { c: Char -> c.isDigit().or(c == ':') }.and(textWithoutCommand.isNotEmpty())){
+
+        if (textWithoutCommand.isNotEmpty().and(textWithoutCommand.all { c: Char -> c.isDigit().or(c == ':') })) {
             val listOfTimes: List<Long> = textWithoutCommand.split(":").map { s: String -> s.toLong() }
-            var secondsToWait: Long = 0
-            if (listOfTimes.size == 3) {
-                val h = listOfTimes.get(0)
-                val m = listOfTimes.get(1)
-                val s = listOfTimes.get(2)
-                if (h >= 0 && h < 24
-                        && m >= 0 && m < 60
-                        && s >= 0 && s < 60){
-                    secondsToWait += listOfTimes.get(0) * 3600 * 1000
-                    secondsToWait += listOfTimes.get(1) * 60 * 1000
-                    secondsToWait += listOfTimes.get(2) * 1000
-                    val acceptingResponse = "I will wait for " + secondsToWait / 1000 + " seconds!"
-                    sendMessage(chatId, acceptingResponse)
-                    Timer("timer", false).schedule(secondsToWait) {
-                        val response = "I've been waiting for " + secondsToWait / 1000 + " seconds!"
-                        sendMessage(chatId, response)
-                    }
-                } else {
-                    val response = "Wrong format of time. You should use hh:mm:ss or just quantity of seconds I should wait"
-                    sendMessage(chatId, response)
-                }
-            } else if (listOfTimes.size == 2){
-                val m = listOfTimes.get(0)
-                val s = listOfTimes.get(1)
-                if (m >= 0 && m < 60
-                        && s >= 0 && s < 60){
-                    secondsToWait += listOfTimes.get(0) * 60 * 1000
-                    secondsToWait += listOfTimes.get(1) * 1000
-//                    sleep(secondsToWait)
-                    val acceptingResponse = "I will wait for " + secondsToWait / 1000 + " seconds!"
-                    sendMessage(chatId, acceptingResponse)
-                    Timer("timer", false).schedule(secondsToWait) {
-                        val response = "I've been waiting for " + secondsToWait / 1000 + " seconds!"
-                        sendMessage(chatId, response)
-                    }
-                } else {
-                    val response = "Wrong format of time. You should use hh:mm:ss or just quantity of seconds I should wait"
-                    sendMessage(chatId, response)
-                }
+
+            val seconds: Long = countSeconds(listOfTimes)
+            if (seconds == (-1).toLong()) {
+                denyWaiting(chatId)
             } else {
-                secondsToWait +=listOfTimes.get(0) * 1000
-//                sleep(secondsToWait)
-                val acceptingResponse = "I will wait for " + secondsToWait / 1000 + " seconds!"
-                sendMessage(chatId, acceptingResponse)
-                Timer("timer", false).schedule(secondsToWait) {
-                    val response = "I've been waiting for " + secondsToWait / 1000 + " seconds!"
-                    sendMessage(chatId, response)
-                }
+                acceptWaiting(chatId, seconds)
+                finishWaiting(chatId, seconds)
             }
         } else {
-            val response = "Wrong format of time. You should use hh:mm:ss or just quantity of seconds I should wait"
-            sendMessage(chatId, response)
+            denyWaiting(chatId)
         }
     } catch (e: UnirestException) {
         logger.log(Level.SEVERE, "Can not wait!", e)
     }
 
+
+    /**
+     * chatId - id of chat
+     * text - text for bot to write to chat
+     *
+     * bot sends message with text to chat of chatId
+     */
     @Throws(UnirestException::class)
     private fun sendMessage(chatId: Long, text: String) {
         Unirest.post(API_ENDPOINT + token + "/sendMessage")
                 .field("chat_id", chatId)
                 .field("text", text)
                 .asJson()
+    }
+
+    /**
+     * listOfTimes: list with hours minutes and seconds (some of it may be absent)
+     *
+     * check if time is sent in the correct format
+     * (no more than 3 elements, hours in range 0..23, minutes and seconds in range 0..59,
+     * if there is only one element is list then it counts as seconds and should be in range 0..oo)
+     * and counts amount of seconds
+     *
+     * returns amount of seconds or -1 if format is not correct
+     */
+    private fun countSeconds(listOfTimes: List<Long>): Long {
+        var secondsToWait: Long = 0
+        var h: Long = 0
+        var m: Long = 0
+        var s: Long = 0
+        var wrongFormat = false
+
+        if (listOfTimes.size == 3) {
+            if (listOfTimes[0] in 0..23
+                    && listOfTimes[1] in 0..59
+                    && listOfTimes[2] in 0..59) {
+                h = listOfTimes[0]
+                m = listOfTimes[1]
+                s = listOfTimes[2]
+            } else {
+                wrongFormat = true
+            }
+        } else if (listOfTimes.size == 2) {
+            if (listOfTimes[0] in 0..59
+                    && listOfTimes[1] in 0..59) {
+                m = listOfTimes[0]
+                s = listOfTimes[1]
+            } else {
+                wrongFormat = true
+            }
+        } else if (listOfTimes.size == 1) {
+            if (listOfTimes[0] >= 0) {
+                s = listOfTimes[0]
+            } else {
+                wrongFormat = true
+            }
+        } else {
+            wrongFormat = true
+        }
+
+        if (wrongFormat) {
+            return -1
+        }
+
+        secondsToWait += h * 3600 * 1000
+        secondsToWait += m * 60 * 1000
+        secondsToWait += s * 1000
+        return secondsToWait / 1000
+    }
+
+
+    /**
+     * bot sends message with acceptance of waiting
+     */
+    private fun acceptWaiting(chatId: Long, secondsToWait: Long) {
+        val response = "I will wait for " + secondsToWait + " seconds!"
+        sendMessage(chatId, response)
+    }
+
+
+    /**
+     * bot sends message of finishing of waiting
+     */
+    private fun finishWaiting(chatId: Long, secondsToWait: Long) {
+        Timer("timer", false).schedule(secondsToWait) {
+            val response = "I've been waiting for " + secondsToWait + " seconds!"
+            sendMessage(chatId, response)
+        }
+    }
+
+
+    /**
+     * bot sends message with denial of waiting
+     */
+    private fun denyWaiting(chatId: Long) {
+        val response = "Wrong format of time. You should use hh:mm:ss or just quantity of seconds I should wait"
+        sendMessage(chatId, response)
     }
 }
--- src/main/kotlin/org/telegram/seconds_bot/Greeting.kt
+++
@@ -1,3 +0,0 @@
-package org.telegram.seconds_bot
-
-data class Greeting(val id: Long, val content: String)
